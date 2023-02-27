35165c47476d432b9043d70a27331b0b33014ad3
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-02-27 13:51:45 -0800

    add toast notifications

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,11 +1,10 @@
 import { redirect } from "react-router-dom";
-
-//helpers
+import { toast } from "react-toastify";
 import { deleteItem } from "../helpers";
 
 export async function logoutAction() {
   // delete the user
   deleteItem({ key: "userName" });
-  // return redirect
+  toast.success("You've deleted your account!");
   return redirect("/");
 }
