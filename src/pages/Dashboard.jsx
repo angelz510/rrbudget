ed6c41eee8bb0a9b9b83704f5bff4d073defb2f3
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-02-27 14:19:23 -0800

    create login form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
-// helper functions
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helpers";
+import Intro from "../components/Intro";
+import { toast } from "react-toastify";
 
 // loader
 export function dashboardLoader() {
@@ -8,14 +9,21 @@ export function dashboardLoader() {
   return { userName };
 }
 
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData();
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 
 export default Dashboard;
