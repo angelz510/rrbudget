2702acdc74c34f68fc8d9882b70ed3cdfb864abc
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-01 10:23:34 -0800

    form clears using reset

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,11 @@ export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
 };
 
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
+
 // create budget
 export const createBudget = ({ name, amount }) => {
   const newItem = {
@@ -10,6 +15,7 @@ export const createBudget = ({ name, amount }) => {
     name: name,
     createdAt: Date.now(),
     amount: +amount,
+    color: generateRandomColor(),
   };
   const existingBudgets = fetchData("budgets") ?? [];
   return localStorage.setItem(
