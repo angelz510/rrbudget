dbd99d3b0a2a697a250d439b90f2e89713b028bb
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-07 00:01:12 -0800

    creating recent expenses table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,13 +4,15 @@ import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
 import BudgetItem from "../components/BudgetItem";
+import Table from "../components/Table";
 import { toast } from "react-toastify";
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
+  const expenses = fetchData("expenses");
   const budgets = fetchData("budgets");
-  return { userName, budgets };
+  return { userName, budgets, expenses };
 }
 
 // action
@@ -57,7 +59,7 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData();
+  const { userName, budgets, expenses } = useLoaderData();
   return (
     <>
       {userName ? (
@@ -78,6 +80,16 @@ const Dashboard = () => {
                     <BudgetItem key={budget.id} budget={budget} />
                   ))}
                 </div>
+                {expenses && expenses.length > 0 && (
+                  <div className="grid md">
+                    <h2>Recent Expenses</h2>
+                    <Table
+                      expenses={expenses.sort(
+                        (a, b) => b.createdAt - a.createdAt
+                      )}
+                    />
+                  </div>
+                )}
               </div>
             ) : (
               <div className="grid-sm">
