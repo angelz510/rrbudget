02a16391c594c87fe995dff4eebe2877332f71cc
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-07 00:12:47 -0800

    now account deletion also deletes budgets and expenses, not just userName

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -5,6 +5,10 @@ import { deleteItem } from "../helpers";
 export async function logoutAction() {
   // delete the user
   deleteItem({ key: "userName" });
+  // delete budgets
+  deleteItem({ key: "budgets" });
+  // delete expenses
+  deleteItem({ key: "expenses" });
   toast.success("You've deleted your account!");
   return redirect("/");
 }
