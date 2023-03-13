d54ac79fddb10fcac22fafc2756cc41d7f00360d
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-13 13:44:44 -0700

    deleteItem function running on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import ExpensesPage, {
   expensesAction,
   expensesLoader,
 } from "./pages/ExpensesPage";
-import BudgetPage, { budgetLoader } from "./pages/BudgetPage";
+import BudgetPage, { budgetAction, budgetLoader } from "./pages/BudgetPage";
 
 const router = createBrowserRouter([
   {
@@ -38,12 +38,14 @@ const router = createBrowserRouter([
         element: <ExpensesPage />,
         loader: expensesLoader,
         action: expensesAction,
+        errorElement: <Error />,
       },
       {
         path: "budget/:id",
         element: <BudgetPage />,
         loader: budgetLoader,
-        // action: expensesAction,
+        action: budgetAction,
+        errorElement: <Error />,
       },
       {
         path: "logout",
--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -1,5 +1,9 @@
-import { getAllMatchingItems } from "../helpers";
+import { deleteItem, getAllMatchingItems } from "../helpers";
 import { useLoaderData } from "react-router-dom";
+import BudgetItem from "../components/BudgetItem";
+import AddExpenseForm from "../components/AddExpenseForm";
+import Table from "../components/Table";
+import { toast } from "react-toastify";
 
 // loader
 export async function budgetLoader({ params }) {
@@ -9,16 +13,58 @@ export async function budgetLoader({ params }) {
     value: params.id,
   })[0];
 
+  const expenses = await getAllMatchingItems({
+    category: "expenses",
+    key: "budgetId",
+    value: params.id,
+  });
+
   if (!budget) {
     throw new Error("The budget you're trying to find doesn't exist.");
   }
+  return { budget, expenses };
+}
 
-  return { budget };
+// action
+export async function budgetAction({ request }) {
+  const data = await request.formData();
+  const { _action, ...values } = Object.fromEntries(data);
+
+  if (_action === "deleteExpense") {
+    try {
+      deleteItem({
+        key: "expenses",
+        id: values.expenseId,
+      });
+      return toast.success("Expense deleted!");
+    } catch (e) {
+      throw new Error("There was a problem deleting your expenses.");
+    }
+  }
 }
 
 const BudgetPage = () => {
-  const { budget } = useLoaderData();
-  return <div>{budget.name}</div>;
+  const { budget, expenses } = useLoaderData();
+  return (
+    <div className="grid-lg" style={{ "--accent": budget.color }}>
+      <h1 className="h2">
+        <span className="accent">{budget.name}</span> Overview
+      </h1>
+      <div className="flex-lg">
+        <BudgetItem budget={budget} />
+        <AddExpenseForm budgets={[budget]} />
+      </div>
+      {expenses && expenses.length > 0 && (
+        <div className="grid-md">
+          <h2>
+            <span className="accent">{budget.name}</span>
+            Expenses
+          </h2>
+          <Table expenses={expenses} showBudget={false} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BudgetPage;
